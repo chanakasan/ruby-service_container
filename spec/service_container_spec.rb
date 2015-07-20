4f6a6d98782f164ead33be00d126bb78bd2972d1
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-07-20 07:25:59 +0530

    allow registering services

--- spec/service_container_spec.rb
+++ spec/service_container_spec.rb
@@ -1,7 +1,15 @@
 require_relative '../lib/service_container'
 
+class MyLogger; end
+
 RSpec.describe ServiceContainer do
   it 'instantiates without errors' do
     expect(subject).not_to be_nil
+  end
+
+  it 'registers a service' do
+    subject.set(:logger) { MyLogger.new }
+
+    expect(subject.get(:logger)).to be_kind_of(MyLogger)
   end
 end

5cfb7d2fbb81d1223f227d2d8192484de14c3ea5
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-07-20 07:52:40 +0530

    register service with dependencies

--- lib/service_container.rb
+++ lib/service_container.rb
@@ -8,6 +8,12 @@
 # i.e get(:your_service_name) method.
 #
 class ServiceContainer
+  # Thrown when a service cannot be located by name.
+  MissingServiceError = Class.new(StandardError)
+
+  # Thrown when a duplicate service is registered.
+  DuplicateServiceError = Class.new(StandardError)
+
   def initialize
     @services = {}
   end
@@ -17,19 +23,19 @@ class ServiceContainer
   # used as the name of a service.
   def set(name, &block)
     if @services[name]
-      fail DuplicateServiceError, "Duplicate Service Name '#{name}'"
+      fail DuplicateServiceError, "A service already exists with name '#{name}'"
     end
     @services[name] = block
   end
 
   def get(name)
-    service_block(name).call
+    service_block(name).call(self)
   end
 
   # Return the block that creates the named service.  Throw an
   # exception if no service creation block of the given name can be
   # found in the container
   def service_block(name)
-    @services[name]
+    @services[name] || fail(MissingServiceError, "No service is registered with the name '#{name}'")
   end
 end
--- spec/service_container_spec.rb
+++ spec/service_container_spec.rb
@@ -1,6 +1,12 @@
 require_relative '../lib/service_container'
 
+# test doubles
 class MyLogger; end
+class DBConnection
+  def initialize(logger)
+    @logger = logger
+  end
+end
 
 RSpec.describe ServiceContainer do
   it 'instantiates without errors' do
@@ -11,5 +17,14 @@ RSpec.describe ServiceContainer do
     subject.set(:logger) { MyLogger.new }
 
     expect(subject.get(:logger)).to be_kind_of(MyLogger)
+  end
+
+  it 'registers a service with dependencies' do
+    subject.set(:logger) { MyLogger.new }
+    subject.set(:db_connection) do |c|
+      DBConnection.new(c.get(:logger))
+    end
+
+    expect(subject.get(:db_connection)).to be_kind_of(DBConnection)
   end
 end
